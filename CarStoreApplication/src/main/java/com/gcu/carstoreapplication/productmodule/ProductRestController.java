2e7ada4151157419eb723e683ea03dd348484fad
Tyler <tfriesen2@my.gcu.edu>
2025-04-18 11:53:27 -0700

    added unsecured API routes

--- CarStoreApplication/src/main/java/com/gcu/carstoreapplication/productmodule/ProductRestController.java
+++ CarStoreApplication/src/main/java/com/gcu/carstoreapplication/productmodule/ProductRestController.java
@@ -1,21 +1,31 @@
 package com.gcu.carstoreapplication.productmodule;
 
+import com.gcu.carstoreapplication.data.ProductDataService;
+import com.gcu.carstoreapplication.model.ProductModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class ProductRestController {
-	
-	// public open mapping link
-    @GetMapping("/")
-    public String home() {
-        return "Unsecure";
+
+    @Autowired
+    private ProductDataService productService;
+
+    // Get all products
+    @GetMapping("/products")
+    public List<ProductModel> getAllProducts() {
+        return productService.findAll();
     }
 
-    // secure mapping link
-    @GetMapping("/secure")
-    public String secure() {
-        return "Secure";
+    // Get a product by ID
+    @GetMapping("products/{id}")
+    public ProductModel getProductById(@PathVariable int id) {
+        return productService.findById(id);
     }
-
 }
